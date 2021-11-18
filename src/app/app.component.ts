cd165c1f6c4972435aad183bdb70d591143b30a5
Giordano Paolo <p.giordano.1320@gmail.com>
2021-11-18 11:50:23 +0100

    [7] Added Events on app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-recipes';
+  title = 'My Recipes';
+  loadedFeature:string = 'recipes';
+
+  onNavigate(feature:string){
+    this.loadedFeature = feature;
+    console.log(this.loadedFeature);
+  }
 }
