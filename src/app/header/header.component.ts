683244c06309ee159208ca0f8648296d482741c5
Giordano Paolo <p.giordano.1320@gmail.com>
2022-01-20 11:25:52 +0100

    [13] Finisced routes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,16 +6,11 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  @Output() featureSelected = new EventEmitter<string>();
 
   collapsed: boolean = true;
 
   constructor() { }
 
   ngOnInit(): void { }
-
-  onSelect(feature: string) {
-    this.featureSelected.emit(feature);
-  }
 
 }
